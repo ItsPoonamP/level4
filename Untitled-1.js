d8d3ae37e5b07d241f4b39a290f5d18b2a384e78
ItsPoonamP <147233308+ItsPoonamP@users.noreply.github.com>
2024-01-05 21:09:12 +0530

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,1 +1,14 @@
-{ font}
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+function opentab(tabname)
+{
+    for (const tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    
+    }
+    for (const tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
